c66e02e24829a6424da8e304eec4616c1470b884
XUEYI.XIA <xtw007@126.com>
2022-10-29 16:18:51 +0800

    增加测试数据

--- app/src/main/java/com/framework/tagflow/MainActivity.kt
+++ app/src/main/java/com/framework/tagflow/MainActivity.kt
@@ -2,6 +2,8 @@ package com.framework.tagflow
 
 import android.os.Bundle
 import android.view.View
+import com.framework.tagflow.adapter.SearchHistoryGridAdapter
+import com.framework.tagflow.adapter.SearchHistoryLinearAdapter
 import com.framework.tagflow.adapter.TestAdapter
 import com.framework.tagflow.bean.SearchHistoryBean
 import com.framework.tagflow.databinding.ActivityMainBinding
@@ -9,6 +11,14 @@ import com.framework.viewbinding.base.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     private lateinit var mTestAdapter : TestAdapter
+
+    private lateinit var mSearchHistoryGridAdapter: SearchHistoryGridAdapter
+
+    private lateinit var mSearchHistoryLinearAdapter: SearchHistoryLinearAdapter
+
+    private val mGridSpace:Int=32; //九宫格分割线间隙
+
+    private val mGridSpanCount=3;//九宫格每一行多少个
 
     override fun initView(rootView: View, savedInstanceState: Bundle?) {
         mViewBinding.multiGridRecyclerTagTitle.text = "RecyclerView 演示(Grid)"
@@ -22,5 +32,22 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
             mTestAdapter.addData(bean)
         }
         mViewBinding.multiFlowTag.setAdapter(mTestAdapter)
+
+
+        mViewBinding.multiGridRecyclerTag.getRecyclerView().addItemDecoration(GridLayoutItemDecoration(mGridSpanCount,mGridSpace, false))
+        mSearchHistoryGridAdapter=SearchHistoryGridAdapter()
+        for(index in 0..10){
+            var bean= SearchHistoryBean()
+            mSearchHistoryGridAdapter.addData(bean)
+        }
+        mViewBinding.multiGridRecyclerTag.setAdapter(mSearchHistoryGridAdapter)
+
+
+        mSearchHistoryLinearAdapter=SearchHistoryLinearAdapter()
+        for(index in 0..10){
+            var bean= SearchHistoryBean()
+            mSearchHistoryLinearAdapter.addData(bean)
+        }
+        mViewBinding.multiLinearRecyclerTag.setAdapter(mSearchHistoryLinearAdapter)
     }
 }
---
+++ app/src/main/java/com/framework/tagflow/adapter/SearchHistoryGridAdapter.kt
@@ -0,0 +1,13 @@
+package com.framework.tagflow.adapter
+
+import com.framework.tagflow.R
+import com.framework.tagflow.bean.SearchHistoryBean
+import com.framework.tagflow.viewholder.BaseQuickAdapter
+import com.framework.tagflow.viewholder.BaseViewHolder
+
+class SearchHistoryGridAdapter : BaseQuickAdapter<SearchHistoryBean, BaseViewHolder>(R.layout.layout_grid_item) {
+
+    override fun convert(holder: BaseViewHolder, item: SearchHistoryBean) {
+
+    }
+}
